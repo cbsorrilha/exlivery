27ed0f7c39016c74509a0e7a04425f4039ddfa8d
Cesar de Barros <cesar.de.b.sorrilha@accenture.com>
2021-05-26 22:01:32 -0300

    feat: report and tests

--- lib/orders/agent.ex
+++ lib/orders/agent.ex
@@ -16,9 +16,11 @@ defmodule Exlivery.Orders.Agent do
 
   defp update_state(state, %Order{} = order, uuid), do: Map.put(state, uuid, order)
 
-  def get(uuid), do: Agent.get(__MODULE__, &get_user(&1, uuid))
+  def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
 
-  defp get_user(state, uuid) do
+  def list_all, do: Agent.get(__MODULE__, & &1)
+
+  defp get_order(state, uuid) do
     case Map.get(state, uuid) do
       nil -> {:error, "order not found"}
       order -> {:ok, order}
